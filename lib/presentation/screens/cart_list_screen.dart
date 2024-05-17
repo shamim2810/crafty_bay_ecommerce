94693f8c8b906fac47a30bfe881e0307e05ae877
shamim2810 <shamim149120@gmail.com>
2024-05-17 11:35:00 +0600

    Cart List

--- lib/presentation/screens/cart_list_screen.dart
+++ lib/presentation/screens/cart_list_screen.dart
@@ -1,6 +1,10 @@
 import 'package:crafty_bay_ecommerce/presentation/state_holders/main_bottom_nav_bar_controller.dart';
+import 'package:crafty_bay_ecommerce/presentation/utility/app_colors.dart';
+import 'package:crafty_bay_ecommerce/presentation/utility/assets_path.dart';
+import 'package:crafty_bay_ecommerce/presentation/widgets/cart_product_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
 
 class CartListScreen extends StatefulWidget {
   const CartListScreen({super.key});
@@ -18,20 +22,79 @@ class _CartListScreenState extends State<CartListScreen> {
       //   return false;
       // },
       canPop: false,
-      onPopInvoked: (_){
+      onPopInvoked: (_) {
         Get.find<MainBottomNavBarController>().backToHome();
       },
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Carts'),
           leading: IconButton(
-              onPressed: (){
+              onPressed: () {
                 Get.find<MainBottomNavBarController>().backToHome();
               },
-              icon: const Icon(Icons.arrow_back_ios_sharp)
-          ),
+              icon: const Icon(Icons.arrow_back_ios_sharp)),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const CartProductItem();
+                },
+              ),
+            ),
+            _buildCheckoutWidget(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCheckoutWidget() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: AppColors.primaryColor.withOpacity(0.1),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildTotalPriceWidget(),
+          SizedBox(
+            width: 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Checkout'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTotalPriceWidget() {
+    return const Column(
+      children: [
+        Text(
+          'Total Price',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+        Text(
+          '\$1200',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: AppColors.primaryColor,
+            fontSize: 24,
+          ),
+        ),
+      ],
     );
   }
 }
